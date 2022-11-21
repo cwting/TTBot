32a4e1857f3bba2d977da6670f426617a27fe96f
cwting <78128212+cwting@users.noreply.github.com>
2022-11-21 22:33:39 +0800

    added about.js

---
+++ commands/about.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(`about`)
+    .setDescription(`About TTBot`)
+    .setDMPermission(true),
+
+  async execute(interaction) {
+    await interaction.reply(`TTBot is made by TingTongBell~ Feel free  to leave any feedback on discord @Ting#4335.`);
+  },
+};
